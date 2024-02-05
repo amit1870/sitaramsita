3863534338b89b1cb99eb75df380cd15a297da02
amit1870 <amitxvf@gmail.com>
2024-02-05 19:13:45 +0530

    sitaram : setting a readme for help

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # [सीताराम](https://amitxvf.pythonanywhere.com/)
 ![सीताराम](accounts/static/accounts/img/sitaram.jpg)
+
+## [Account Creation]
+
+### create a google account with similar to like dukan
+
+### create a pythonanywhere.com account with the email
+
+###  change system image in pythonanywere.com account setting
+
+### create a account with fast2sms.com for message otp
+
+###  get secret key with you that will be used in wsgi setup
+
+
+## [PythonAnyWhere console]
+
+### run setup.sh commands with no error
+
+
+## [wsgi file change and reload]
+
+```
+import os
+import sys
+
+HOME = os.environ['HOME']
+CODE_PATH = f'{HOME}/sitaramsita'
+
+if CODE_PATH not in sys.path:
+    sys.path.insert(0, CODE_PATH)
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "sitaramsita.settings"
+os.environ["SECRET_KEY"] = "WhDCWhDCaQSfX5YmxhDCaQSfX5YKubtTASfX5YmxZw2qlHKubtTA"
+os.environ["FAST_SMS_AUTH_KEY"] = "SMS_AUTH_SECRET_KEY"
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+```
+
+# सीताराम
--- setup.sh
+++ setup.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
-USER=$1
-AUTH_KEY=$2
-echo "set up started for ${USER}.pythonanywhere.com ....\n"
 
 # go to $HOME dir
 cd $HOME
 
 # create virtual env for package install
 mkvirtualenv --python /usr/bin/python3.8 vrenv
-
-if [ "$?" -ne "0" ]; then
-    echo "some error occured during virtual env creation ...\n"
-    echo "stopping ..."
-    exit
-fi
 
 # activate virtual env
 workon vrenv
@@ -27,83 +18,27 @@ git clone https://ghp_b2VYWGWEwKQxAIOIOHe4ivNMsyWtsx0ZGaUY@github.com/amit1870/s
 cd sitaramsita
 
 # install requirements
-pip install -q -r requires.txt
+pip install -r requires.txt
 
 # run migration commands
 python manage.py makemigrations accounts dukan
 
-if [ "$?" -ne "0" ]; then
-    echo "some error occured during creating migrations ...\n"
-    echo "stopping ..."
-    exit
-fi
-
 # run migrate command if no error in pevious command
 python manage.py migrate
 
-if [ "$?" -ne "0" ]; then
-    echo "some error occured during sql migrations ...\n"
-    echo "stopping ..."
-    exit
-fi
+# run command to create superuser
+python manage.py createsuper
 
 # create static and media dir
 mkdir -p static media
 
-if [ "$?" -ne "0" ]; then
-    echo "some error occured during dir creation ...\n"
-    echo "stopping ..."
-    exit
-fi
-
 # collect static files
 python manage.py collectstatic
-
-if [ "$?" -ne "0" ]; then
-    echo "some error occured during setup ...\n"
-    echo "stopping ..."
-    exit
-fi
 
 # go to $HOME dir
 cd $HOME
 
-# empty wsgi file
-WSGI_FILE="/var/www/${USER}_pythonanywhere_com_wsgi.py"
-touch $WSGI_FILE
-> $WSGI_FILE
-
-if [ "$?" -ne "0" ]; then
-    echo "some error occured during empting ${USER}_pythonanywhere_com_wsgi.py ...\n"
-    echo "stopping ..."
-    exit
-fi
-
-# add content to wsgi file
-echo "import os" >> $WSGI_FILE
-echo "import sys" >> $WSGI_FILE
-echo "HOME = os.environ['HOME']" >> $WSGI_FILE
-echo "CODE_PATH = f'{HOME}/sitaramsita'" >> $WSGI_FILE
-echo "if CODE_PATH not in sys.path:" >> $WSGI_FILE
-echo "    sys.path.insert(0, CODE_PATH)" >> $WSGI_FILE
-echo "os.environ['DJANGO_SETTINGS_MODULE'] = 'sitaramsita.settings'" >> $WSGI_FILE
-echo "os.environ['SECRET_KEY'] = 'WhDCWhDCaQSfX5YmxhDCaQSfX5YKubtTASfX5YmxZw2qlHKubtTA'" >> $WSGI_FILE
-echo "os.environ['FAST_SMS_AUTH_KEY'] = '${AUTH_KEY}'" >> $WSGI_FILE
-echo "from django.core.wsgi import get_wsgi_application" >> $WSGI_FILE
-echo "application = get_wsgi_application()"
-echo "" >> $WSGI_FILE
-
-echo "wsgi file set up finished."
-
+# reload app
 touch $WSGI_FILE
 
-echo "reloading ${WSGI_FILE} ..."
-
-
-echo "create superuser manually."
-
-echo "console set up finished successfully for ${USER}.pythonanywhere.com ....\n"
-
-echo "finished !!"
-
-
+# create a task
